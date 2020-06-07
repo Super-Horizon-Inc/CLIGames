034acd19862a59e625277571bbbab3175afb0fe8
Dang Nguyen <nguyenminhdang1995@gmail.com>
2020-06-06 23:00:30 -0500

    generated javadoc files

--- BlackKnight/src/main/java/core/model/Position.java
+++ BlackKnight/src/main/java/core/model/Position.java
@@ -2,6 +2,9 @@ package core.model;
 
 import javax.persistence.*;
 
+/**
+ * A class for position of Character
+ */
 @Embeddable
 public class Position {
 
@@ -20,18 +23,36 @@ public class Position {
         this.y = y;
     }
 
+    /**
+     * Setter method for x-coordinate of position. This method can also be used as a combination of getter and setter.
+     * @param x a byte number which is assigned to Position x-coordinate.
+     * @return First, assign x-coordinate to Position x. Then, return a byte number which is Position x-coordinate.
+     */
     public byte setX(byte x) {
         return this.x = x;
     }
 
+    /**
+     * Getter method for x-coordinate Position.
+     * @return a byte number which is x-coordinate Position.
+     */
     public byte getX() {
         return this.x = x;
     }
 
+    /**
+     * Setter method for y-coordinate of position. This method can also be used as a combination of getter and setter.
+     * @param y a byte number which is assigned to Position y-coordinate.
+     * @return First, assign y-coordinate to Position y. Then, return a byte number which is Position y-coordinate.
+     */
     public byte setY(byte y) {
         return this.y = y;
     }
 
+    /**
+     * Getter method for y-coordinate Position.
+     * @return a byte number which is y-coordinate Position.
+     */
     public byte getY() {
         return this.y = y;
     }
